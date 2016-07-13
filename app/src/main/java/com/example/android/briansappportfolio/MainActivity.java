6e456dbc8de14e17db035d655bb68fb1c5843732
Brian <sourpatchswag@yahoo.com>
2016-07-12 18:30:31 -0700

    Second Commit

--- app/src/main/java/com/example/android/briansappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/briansappportfolio/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,29 +13,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void popularMoviesApp (View view) {
-      Toast.makeText(MainActivity.this, "This button will launch the popular movies app", Toast.LENGTH_SHORT).show();
-        return;
+
+    private void showToastMessage(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
-    public void stock_hawk_app (View view) {
-        Toast.makeText(MainActivity.this, "This button will launch the Stock Hawk App", Toast.LENGTH_SHORT).show();
-        return;
+//Temporarily displays a toast message, inventially will house an intent for the Popular Movies App
+    public void popularMoviesApp(View view) {
+        showToastMessage("This button will launch the Popular Movies App");
     }
-    public void buildItBigger (View view) {
-        Toast.makeText(MainActivity.this, "This button will launch the Build It Bigger App", Toast.LENGTH_SHORT).show();
-        return;
+    //Temporarily displays a toast message, inventially will house an intent for the Stock Hawk App
+    public void stock_hawk_app(View view) {
+        showToastMessage("This button will launch the Stock Hawk App");
     }
-    public void makeAppMaterial (View view) {
-        Toast.makeText(MainActivity.this, "This button will launch the Make Your App Material App", Toast.LENGTH_SHORT).show();
-        return;
+    //Temporarily displays a toast message, inventially will house an intent for the Build It Bigger App
+    public void buildItBigger(View view) {
+        showToastMessage("This button will launch the Build It Bigger App");
     }
-    public void goUbiquitous (View view) {
-        Toast.makeText(MainActivity.this, "This button will launch the Go Ubiquitous App", Toast.LENGTH_SHORT).show();
-        return;
+    //Temporarily displays a toast message, inventially will house an intent for the Make Your App Material
+    public void makeAppMaterial(View view) {
+        showToastMessage("This button will launch the Make Your App Material App");
     }
-    public void capstone (View view) {
-        Toast.makeText(MainActivity.this, "This button will launch the Capstone App", Toast.LENGTH_SHORT).show();
-        return;
+    //Temporarily displays a toast message, inventially will house an intent for the Go Ubiquitous App
+    public void goUbiquitous(View view) {
+        showToastMessage("This button will launch the Go Ubiquitous App");
+    }
+    //Temporarily displays a toast message, inventially will house an intent for the Custom Capstone App
+    public void capstone(View view) {
+        showToastMessage("This button will launch the Capstone App");
     }
 }
 
